1f33fe5a19b5daf390e2bd866caf3f78a8377342
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2023-12-10 17:46:09 +0100

    Finished Project
    Hardish 90 min

--- starter/class/connect-four.js
+++ starter/class/connect-four.js
@@ -21,18 +21,115 @@ class ConnectFour {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', ConnectFour.testCommand);
+    Screen.addCommand('d', 'Press d to go Rsight', 
+    () => { this.cursorMove(this.cursor.right); });
 
-    this.cursor.setBackgroundColor();
+    Screen.addCommand('a', 'Press a to go Left',
+    () => { this.cursorMove(this.cursor.left); });
+
+    Screen.addCommand('j', 'Press j to place move',
+     () => { this.placeMove(); });
+    
     Screen.render();
   }
 
-  // Remove this
-  static testCommand() {
-    console.log("TEST COMMAND");
+  cursorMove(cursorMoveFunction) {
+    cursorMoveFunction.call(this.cursor);
+    Screen.render();
+  }
+  placeMove() {
+    const cursorCol = this.cursor.col;
+
+    // Find the first available row from the bottom of the column
+    let row;
+    for (row = this.grid.length - 1; row >= 0; row--) {
+      if (this.grid[row][cursorCol] === ' ') {
+        this.grid[row][cursorCol] = this.playerTurn;
+        // Use Screen.setGrid to update the screen with the new move
+        Screen.setGrid(row, cursorCol, this.playerTurn);
+        break;
+      }
+    }
+
+    // If the column is full, do nothing
+    if (row === -1) {
+      return;
+    }
+
+    // Alternate player turns between 'X' and 'O'
+    this.playerTurn = this.playerTurn === 'X' ? 'O' : 'X';
+
+    // Check for a win or tie
+    const result = ConnectFour.checkWin(this.grid);
+    if (result) {
+      ConnectFour.endGame(result);
+    }
+
+    // Render the updated grid
+    Screen.render();
   }
 
+
+
   static checkWin(grid) {
+    // check for horizantal wins
+    for (let row = 0; row < grid.length; row++) {
+        for(let col = 0; col < grid[row].length; col++)
+      if (grid[row][col] !== ' ' && 
+          grid[row][col  ] === grid[row][col+1] && 
+          grid[row][col+1] === grid[row][col+2] && 
+          grid[row][col+2] === grid[row][col+3]) {
+        // If a player has 4 consecutive Xs or Os, return the player symbol (X or O)
+        return grid[row][col];
+      }
+    }
+    // check for vertical wins
+    let columnLength = grid[0].length;
+    for (let col = 0; col < columnLength; col++) {
+        for(let row = 0; row < grid.length -3; row++){
+          if (grid[row][col] !== ' ' && 
+              grid[row  ][col] === grid[row+1][col] && 
+              grid[row+1][col] === grid[row+2][col] && 
+              grid[row+2][col] === grid[row+3][col]) {
+            // If a player has 4 consecutive Xs or Os for entire column, return the player symbol (X or O)
+            return grid[row][col];
+          }
+        }
+     
+    }
+    // check for diagonal downwards wins
+    for (let col = 0; col < columnLength; col++) {
+      for (let row = 0; row < grid.length -3; row++) {
+    if (grid[row][col] !== ' ' && 
+        grid[row   ][col  ] === grid[row+1][col+1] && 
+        grid[row +1][col+1] === grid[row+2][col+2] && 
+        grid[row +2][col+2] === grid[row+3][col+3]) {
+      return grid[row][col];
+    }
+     
+      }
+  }
+
+    // Check for diagonal upward wins
+    for (let col = 0; col < columnLength; col++) {
+      for (let row = grid.length -1 ; row >= 3; row--) {
+    if (grid[row][col] !== ' ' && 
+        grid[row  ][col  ] === grid[row-1][col+1] && 
+        grid[row-1][col+1] === grid[row-2][col+2] && 
+        grid[row-2][col+2] === grid[row-3][col+3]) {
+      return grid[row][col];
+    }
+  }
+}
+    // Check for a tie (if there are no empty spaces left)
+    const isTie = grid.every(row => row.every(cell => cell !== ' '));
+    if (isTie) {
+      return 'T'; // Return 'T' for tie
+    }
+
+    // If no winner and the game is not a tie, return false to indicate the game hasn't ended yet
+
+    return false;
 
     // Return 'X' if player X wins
     // Return 'O' if player O wins
--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -11,6 +11,7 @@ class Cursor {
 
     this.gridColor = 'black';
     this.cursorColor = 'yellow';
+    
 
   }
 
@@ -22,12 +23,37 @@ class Cursor {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
 
+  up() {
+    if (this.row > 0) {
+      this.resetBackgroundColor();
+      this.row--;
+      this.setBackgroundColor();
+    }
+  }
+
+  down() {
+    if (this.row < this.numRows - 1) {
+      this.resetBackgroundColor();
+      this.row++;
+      this.setBackgroundColor();
+    }
+  }
   left() {
-    // Move cursor left
+    if (this.col > 0) {
+      this.resetBackgroundColor();
+      this.col--;
+      this.setBackgroundColor();
+    }
   }
 
   right() {
+
     // Move cursor right
+    if (this.col < this.numCols - 1) {
+      this.resetBackgroundColor();
+      this.col++;
+      this.setBackgroundColor();
+    }
   }
 
 }
